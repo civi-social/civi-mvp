3cda6a61963b4bc4debf2f8976f39c3e8c08e7c4
Sartaj <sartaj@sartaj.me>
2022-11-14 23:53:44 -0600

    Clean Up Setup Scripts Around Environment Variables (#21)
    Cleans up set up scripts around environment variables.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
+# Installing Fly Deployment Stuff
 curl -L https://fly.io/install.sh | sh
 export FLYCTL_INSTALL="/home/gitpod/.fly"
 export PATH="$FLYCTL_INSTALL/bin:$PATH"
 
-cp .env.example .env
+# Setting up .env file
+./env-setup
+
+# Prepare
 npm i
 npm run docker
 npm run setup
 npm run build
-
-echo ".env file was generated. Remember to fill out different things needed for full control"
