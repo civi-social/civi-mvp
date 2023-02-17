987d4d57a0e5d5bb4e290cc2bcc0cbc43cb6a482
Sartaj <sartaj@sartaj.me>
2023-02-17 05:19:16 +0000

    update

--- app/entities/locales/usa.ts
+++ app/entities/locales/usa.ts
@@ -5,18 +5,22 @@ import type { LegislationData } from "../legislation";
 export const legiscanResultToUSALegislation = (
   bills: LegiscanBill[]
 ): LegislationData[] => {
-  console.log("USA BILLS");
-  return bills.map((bill): LegislationData => {
-    return {
-      status: STATUS_MAP[bill.status] || "",
-      date: bill.last_action_date,
-      // only get first two sentences
-      description: bill.description,
-      sponsor: "",
-      id: bill.number,
-      title: bill.title,
-      link: bill.url,
-      tags: [],
-    };
-  });
+  return (
+    bills
+      // only show bills that are passed introduction stage
+      .filter((bill) => bill.status > 1)
+      .map((bill): LegislationData => {
+        return {
+          status: STATUS_MAP[bill.status] || "",
+          date: bill.last_action_date,
+          // only get first two sentences
+          description: bill.description,
+          sponsor: "",
+          id: bill.number,
+          title: bill.title,
+          link: bill.url,
+          tags: [],
+        };
+      })
+  );
 };
