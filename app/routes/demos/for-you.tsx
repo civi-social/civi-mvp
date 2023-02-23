ae1453c3278bc69ff111104cd167ce3a98a104e9
Sartaj <sartaj@sartaj.me>
2023-02-23 07:01:45 +0000

    update

--- app/routes/demos/for-you.tsx
+++ app/routes/demos/for-you.tsx
@@ -7,9 +7,8 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import type { Env } from "~/config";
 import { getEnv } from "~/config";
-import { forYouData } from "~/for-you";
+import { ForYou, forYouData } from "~/for-you";
 import type { LegislationData } from "~/legislation";
-import { DataField, ResultCard, Spacing } from "~/ui";
 
 interface LoaderData {
   legislation: LegislationData[];
@@ -40,34 +39,5 @@ export const meta: MetaFunction = ({ data }: { data: LoaderData }) => {
 export default function OfficePage() {
   const { legislation } = useLoaderData<LoaderData>();
 
-  return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div
-        style={{
-          maxWidth: "500px",
-          alignContent: "center",
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        {legislation.map(({ id, title, date, sponsor, link, description }) => (
-          <div key={id + title} style={{ marginTop: Spacing.FOUR }}>
-            <ResultCard
-              title={title}
-              subtitle={description}
-              channels={
-                <>
-                  <DataField type="Text" id={id} />
-                  {sponsor && <DataField type="Text" id={sponsor} />}
-                  <DataField type="Text" id={date} />
-                  <DataField type="URL" id={link} />
-                </>
-              }
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <ForYou legislation={legislation} />;
 }
