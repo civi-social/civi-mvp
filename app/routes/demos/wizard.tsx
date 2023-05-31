7145301d683794980395dfa2c21b72d7b81d0499
Sartaj <sartaj@sartaj.me>
2023-05-31 02:59:47 +0000

    update

--- app/routes/demos/wizard.tsx
+++ app/routes/demos/wizard.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { Bill } from "~app/modules/for-you";
 import { RepLevel } from "~app/modules/levels";
+import { NewsletterForm } from "~app/modules/newsletter-form/NewsletterForm";
 
 const Card: React.FC = ({ children }) => {
   return <div className="bg-white p-4 text-center shadow">{children}</div>;
@@ -193,50 +194,7 @@ const VoteOnBill: React.FC<ScreenProps> = ({ onNextStep }) => {
 };
 
 const SignUp: React.FC<ScreenProps> = ({ onNextStep }) => {
-  return (
-    <section
-      className=" flex flex-col items-center 
-    justify-center p-4"
-    >
-      <h2 className="mb-2 text-center text-xl font-bold">Like what you see?</h2>
-      <h3 className="text-center italic">
-        Sign up! We need 500 people from the 40th Ward on our waiting list
-      </h3>
-      <form
-        className="w-full max-w-sm justify-center"
-        action="https://social.us11.list-manage.com/subscribe/post?u=5d27cdfc23603091d05858a9b&amp;id=e52325cbc0&amp;f_id=00f5a6e0f0"
-        method="post"
-      >
-        <div className="flex items-center border-b border-secondary-400 py-2">
-          <input
-            className="mr-3 w-full appearance-none border-none bg-transparent py-1 px-2 leading-tight text-gray-700 focus:outline-none"
-            type="email"
-            name="EMAIL"
-            placeholder="email address"
-            aria-label="E-mail Address"
-          />
-          <div
-            style={{ position: "absolute", left: "-5000px" }}
-            aria-hidden="true"
-          >
-            <input
-              type="text"
-              name="b_5d27cdfc23603091d05858a9b_e52325cbc0"
-              tabIndex={-1}
-              value=""
-            />
-          </div>
-          <button
-            className="flex-shrink-0 rounded border-4 border-secondary-400 bg-secondary-400 py-1 px-2 text-sm text-zinc-50 hover:border-secondary-200 hover:bg-secondary-200 hover:text-gray-800"
-            type="submit"
-            name="submit"
-          >
-            Sign Up For Our Waitlist
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+  return <NewsletterForm />;
 };
 
 enum WizardStep {
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,8 @@
-import type {
-  MetaFunction
-} from "@remix-run/node";
-import { Link } from "@remix-run/react"
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 import { IntroContent, Progress } from "~app/modules/intro/Intro";
+import { NewsletterForm } from "~app/modules/newsletter-form/NewsletterForm";
 
 interface LoaderData {}
 // export const loader: LoaderFunction = async ({ request }) => {
@@ -30,50 +29,81 @@ export const meta: MetaFunction = ({ data }: { data: LoaderData }) => {
 
 const App = () => {
   return (
-    <div className="bg-gray-100 min-h-screen text-gray-800">
+    <div className="min-h-screen bg-gray-100 text-gray-800">
       <header className="bg-primary-200 p-4">
-        <h1 className="text-2xl font-bold text-center">Civi</h1>
-        <p className="italic text-center">Fostering inclusive, community-based virtual-direct democracy.</p>
+        <h1 className="text-center text-2xl font-bold">Civi</h1>
+        <p className="text-center italic">
+          Fostering inclusive, community-based virtual-direct democracy.
+        </p>
       </header>
       <main className="p-4">
-        <section id="AboutUs" className="bg-zinc-50 rounded-lg shadow-lg p-4 mb-4">
-          <h2 className="text-xl font-bold mb-2">About Us</h2>
-          <p className="mb-2">We are a non-partisan, non-profit, open-source team, focused on connecting community members with their representatives to influence government.</p>
+        <section
+          id="AboutUs"
+          className="mb-4 rounded-lg bg-zinc-50 p-4 shadow-lg"
+        >
+          <h2 className="mb-2 text-xl font-bold">About Us</h2>
+          <p className="mb-2">
+            We are a non-partisan, non-profit, open-source team, focused on
+            connecting community members with their representatives to influence
+            government.
+          </p>
           <p>With Civi, it is easy to </p>
-          <ul className="list-disc list-inside mb-2">
-            <li>
-            find and understand the impact of policy decisions
-            </li>
-            <li>
-            communicate with representatives to influence decisions
-            </li>
+          <ul className="mb-2 list-inside list-disc">
+            <li>find and understand the impact of policy decisions</li>
+            <li>communicate with representatives to influence decisions</li>
           </ul>
         </section>
         <section id="Demo" className="flex flex-col items-center">
-          <h2 className="text-xl font-bold mt-2">Give it a spin!</h2>
+          <h2 className="mt-2 text-xl font-bold">Give it a spin!</h2>
           <p>Explore our initial demo and share any feedback you have.</p>
-          <Link to="/demos/wizard" className="bg-secondary-200 hover:bg-secondary-400 active:bg-zinc-50 rounded-lg shadow-lg p-4 pl-6 pr-6 m-6 w-fit">View our demo.</Link>
+          <Link
+            to="/demos/wizard"
+            className="m-6 w-fit rounded-lg bg-secondary-200 p-4 pl-6 pr-6 shadow-lg hover:bg-secondary-400 active:bg-zinc-50"
+          >
+            View our demo.
+          </Link>
         </section>
-        <section id="SignUp" className=" p-4 flex flex-col 
-                    items-center justify-center">
-          <h2 className="text-xl font-bold mb-2 text-center">Like what you see?</h2>
-          <p className="italic text-center">Embrace the power of your political voice.</p>
-          <p className="italic text-center">Sign up for our mailing list to receive the latest updates.</p>
+        <NewsletterForm />
+        <section
+          id="SignUp"
+          className=" flex flex-col items-center 
+                    justify-center p-4"
+        >
+          <h2 className="mb-2 text-center text-xl font-bold">
+            Like what you see?
+          </h2>
+          <p className="text-center italic">
+            Embrace the power of your political voice.
+          </p>
+          <p className="text-center italic">
+            Sign up for our mailing list to receive the latest updates.
+          </p>
           <form className="w-full max-w-sm justify-center">
             <div className="flex items-center border-b border-secondary-400 py-2 ">
-              <input className="appearance-none bg-transparent w-full text-gray-800 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="email address" aria-label="E-mail Address"/>
-              <button className="flex-shrink-0 bg-secondary-400 hover:bg-secondary-200 border-secondary-400 hover:border-secondary-200 text-sm border-4 text-zinc-50 hover:text-gray-800 py-1 px-2 rounded" type="button">
+              <input
+                className="mr-3 w-full appearance-none bg-transparent py-1 px-2 leading-tight text-gray-800 focus:outline-none"
+                type="text"
+                placeholder="email address"
+                aria-label="E-mail Address"
+              />
+              <button
+                className="flex-shrink-0 rounded border-4 border-secondary-400 bg-secondary-400 py-1 px-2 text-sm text-zinc-50 hover:border-secondary-200 hover:bg-secondary-200 hover:text-gray-800"
+                type="button"
+              >
                 Sign Up
               </button>
             </div>
-          </form>        
+          </form>
         </section>
-        <section id="InProgress" className="bg-zinc-50 rounded-lg shadow-lg p-4 mb-4">
-          <Progress/>
+        <section
+          id="InProgress"
+          className="mb-4 rounded-lg bg-zinc-50 p-4 shadow-lg"
+        >
+          <Progress />
         </section>
       </main>
       <footer className="bg-primary-200 p-4 text-center">
-        <button className="bg-primary-600 hover:bg-secondary-200 text-zinc-50 hover:text-gray-800 font-bold py-2 px-4 rounded">
+        <button className="rounded bg-primary-600 py-2 px-4 font-bold text-zinc-50 hover:bg-secondary-200 hover:text-gray-800">
           Donate
         </button>
       </footer>
