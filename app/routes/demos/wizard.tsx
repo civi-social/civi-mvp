d5c9959f41f560493d8ce6608b252229740eff5b
Sartaj <sartaj@sartaj.me>
2023-05-31 01:48:38 +0000

    progress

--- app/routes/demos/wizard.tsx
+++ app/routes/demos/wizard.tsx
@@ -8,41 +8,33 @@ interface ScreenProps {
   num?: number;
 }
 
-const Screen: React.FC<ScreenProps> = ({ onNextStep, num }) => {
-  return (
-    <div>
-      <h2>Step {num}</h2>
-      {/* Add your form or content for Step 1 here */}
-      <button onClick={onNextStep}>Next</button>
-    </div>
-  );
-};
-
 const BillIntro: React.FC<ScreenProps> = ({ onNextStep }) => {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">
-          Alder Vasquez wants your opinion on this bill?
-        </h1>
-        <p className="text-3xl text-gray-600">
-          Establishes the Illinois Psilocybin Advisory Board{" "}
-        </p>
-        <button
-          onClick={onNextStep}
-          className="mt-4 rounded bg-blue-500 py-2 px-4 text-3xl font-bold text-white hover:bg-blue-700"
-        >
-          Learn More
-        </button>
-        {/* <button
+    <PhoneContainer onNextStep={onNextStep}>
+      <ScreenContainer>
+        <Card>
+          <h1 className="text-2xl font-bold">
+            Alder Vasquez wants your opinion on this bill?
+          </h1>
+          <p className="text-xl text-slate-600">
+            Establishes the Illinois Psilocybin Advisory Board{" "}
+          </p>
+          <button
+            onClick={onNextStep}
+            className="mt-4 rounded bg-blue-500 py-2 px-4 font-bold uppercase text-white hover:bg-blue-700"
+          >
+            Learn More
+          </button>
+          {/* <button
           onClick={onNextStep}
           className="mt-4 rounded bg-blue-500 py-2 px-4 text-3xl font-bold text-white hover:bg-blue-700"
         >
           Whatever You Prefer
         </button>
         <div className="italic">(I Trust You Alder)</div> */}
-      </div>
-    </div>
+        </Card>
+      </ScreenContainer>
+    </PhoneContainer>
   );
 };
 
@@ -111,19 +103,23 @@ const BillInformation: React.FC<ScreenProps> = ({ onNextStep }) => {
 
 const VoteOnBill: React.FC<ScreenProps> = ({ onNextStep }) => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center overflow-y-auto bg-gray-100 text-center">
-      <div className="text-4xl font-bold">
-        We will send Alder Vasquez your vote!
-      </div>
-      <div>We will send a notification once Alder Vasquez votes</div>
-      <div>Want to see a template of what we send?</div>
-      <button
-        onClick={onNextStep}
-        className="mt-4 rounded bg-blue-500 py-2 px-4 text-3xl font-bold text-white hover:bg-blue-700"
-      >
-        One week later
-      </button>
-    </div>
+    <PhoneContainer onNextStep={onNextStep}>
+      <ScreenContainer>
+        <Card>
+          <div className="text-4xl font-bold">
+            We will send Alder Vasquez your vote!
+          </div>
+          <div>We will send a notification once Alder Vasquez votes</div>
+          <div>Want to see a template of what we send?</div>
+          <button
+            onClick={onNextStep}
+            className="mt-4 rounded bg-blue-500 py-2 px-4 text-3xl font-bold text-white hover:bg-blue-700"
+          >
+            One week later
+          </button>
+        </Card>
+      </ScreenContainer>
+    </PhoneContainer>
   );
 };
 
@@ -151,16 +147,16 @@ enum WizardStep {
 
 const App: React.FC = () => {
   const [currentStep, setCurrentStep] = useState<WizardStep>(
-    WizardStep.BillIntroNotification
+    WizardStep.BillIntro
   );
 
   const handleNextStep = () => {
     setCurrentStep((prevStep) => prevStep + 1);
   };
 
-  const handlePrevStep = () => {
-    setCurrentStep((prevStep) => prevStep - 1);
-  };
+  // const handlePrevStep = () => {
+  //   setCurrentStep((prevStep) => prevStep - 1);
+  // };
 
   const renderCurrentStep = () => {
     switch (currentStep) {
@@ -168,8 +164,8 @@ const App: React.FC = () => {
         return (
           <Notification
             onNextStep={handleNextStep}
-            title="Alder Vasquez wants your opinion on this bill."
-            message="It's regarding magic mushrooms."
+            title="Your Legislator is requesting your vote on a bill!"
+            message=""
           />
         );
       case WizardStep.BillIntro:
@@ -203,7 +199,18 @@ const App: React.FC = () => {
     }
   };
 
-  return <div className="container mx-auto">{renderCurrentStep()}</div>;
+  return (
+    <div className="flex min-h-screen justify-center bg-slate-100">
+      <div className="w-full max-w-sm">
+        <div className="container mx-auto p-2">
+          <div className="text-center font-serif text-sm uppercase">
+            The 40th Ward Experiment Demo
+          </div>
+          <div>{renderCurrentStep()}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
@@ -214,27 +221,93 @@ const Notification: React.FC<{
   onNextStep: () => void;
 }> = ({ title, message, onNextStep }) => {
   return (
-    <div className="fixed top-5 left-5 right-5 flex items-center justify-center rounded-md bg-white p-4 shadow-md">
-      <div className="mr-3 flex-shrink-0">
-        <svg
-          className="h-6 w-6 text-gray-500"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 3h0l9 9h0l-9 9h0l-9-9h0l9-9Z"
-          />
-        </svg>
+    <div>
+      <div className="p-2 text-center">
+        You receive the following notification on your phone.
       </div>
-      <div role="button" onClick={onNextStep}>
-        <p className="font-medium text-gray-800">{title}</p>
-        <p className="text-gray-600">{message}</p>
-      </div>
+      <HomeLockScreen>
+        <div className="flex items-center justify-center rounded-md bg-white p-4 shadow-md">
+          <div className="mr-3 flex-shrink-0">
+            <svg
+              className="h-6 w-6 text-gray-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 3h0l9 9h0l-9 9h0l-9-9h0l9-9Z"
+              />
+            </svg>
+          </div>
+          <div role="button" onClick={onNextStep}>
+            <p className="font-medium text-gray-800">{title}</p>
+            <p className="text-gray-600">{message}</p>
+          </div>
+        </div>
+      </HomeLockScreen>
+      <button onClick={onNextStep}>Next</button>
     </div>
+  );
+};
+
+const HomeLockScreen: React.FC = ({ children }) => {
+  return (
+    <div
+      className="rounded-lg p-2"
+      style={{
+        backgroundImage: "url(https://9to5mac.com/?attachment_id=818555)",
+        backgroundSize: "cover",
+        minHeight: "600px",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const PhoneContainer: React.FC<ScreenProps> = ({ onNextStep, children }) => {
+  return (
+    <div>
+      <div
+        className="m-2 rounded-lg bg-slate-900"
+        style={{ height: "500px", overflowY: "auto" }}
+      >
+        <StatusBar />
+        <div className="p-4">{children}</div>
+      </div>
+      <button onClick={onNextStep}>Next</button>
+    </div>
+  );
+};
+
+const StatusBar = () => (
+  <div className="flex items-center justify-between bg-gray-700 p-1">
+    <div className="text-xs text-white">
+      <p>Carrier</p>
+    </div>
+    <div className="flex gap-1">
+      <div className="h-1 w-1 rounded-full bg-white"></div>
+      <div className="h-1 w-1 rounded-full bg-white"></div>
+      <div className="h-1 w-1 rounded-full bg-white"></div>
+      <div className="h-1 w-1 rounded-full bg-white"></div>
+      <div className="h-1 w-1 rounded-full bg-white"></div>
+    </div>
+    <div className="text-xs text-white">
+      <p>9:41 AM</p>
+    </div>
+  </div>
+);
+
+const Card: React.FC = ({ children }) => {
+  return <div className="bg-white p-4 text-center shadow">{children}</div>;
+};
+
+const ScreenContainer: React.FC = ({ children }) => {
+  return (
+    <div className="mt-4 flex items-center justify-center">{children}</div>
   );
 };
