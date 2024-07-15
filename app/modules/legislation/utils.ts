b8ac87de5b5f728ab6ae67b6bbd062580ad5293c
Sartaj <sartaj@sartaj.me>
2024-07-15 05:14:02 -0500

    cleanup

---
+++ app/modules/legislation/utils.ts
@@ -0,0 +1,64 @@
+import { RepLevel } from "../levels";
+
+// TODO: Move to backend
+export const mapToReadableStatus = (
+  level: RepLevel,
+  status: string
+): { name: string; type: "in-progress" | "pass" | "fail" } => {
+  switch (level) {
+    case RepLevel.City:
+      switch (status) {
+        case "introduction":
+          return { name: "Introduced", type: "in-progress" };
+        case "referral-committee":
+          return { name: "In Committee", type: "in-progress" };
+        case "passage":
+          return { name: "Passed", type: "pass" };
+        case "substitution":
+          return { name: "Substituted", type: "in-progress" };
+        case "committee-passage-favorable":
+          return { name: "Recommended By Committee", type: "in-progress" };
+        default:
+          return {
+            name: toTitleCase(status.split("-").join(" ")),
+            type: "in-progress",
+          };
+      }
+    case RepLevel.State:
+      switch (status) {
+        case "Pass":
+          return { name: "Became Law", type: "pass" };
+      }
+    case RepLevel.National:
+      switch (status) {
+        case "Engross":
+          return { name: "Passed House", type: "in-progress" };
+        case "Pass":
+          return { name: "Became Law", type: "pass" };
+      }
+  }
+  return { name: status, type: "in-progress" };
+};
+
+// TODO: We need to clean up the status data on the backend
+export const getLastStatus = (status: unknown): string => {
+  if (typeof status === "string") {
+    try {
+      const parsed = JSON.parse(status);
+      return parsed[parsed.length - 1];
+    } catch (e) {
+      return status;
+    }
+  }
+  if (Array.isArray(status)) {
+    return status[status.length - 1];
+  }
+  return "";
+};
+
+const toTitleCase = (str: string) => {
+  return str.replace(
+    /\w\S*/g,
+    (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+  );
+};
---
+++ app/modules/ui/Carousel/Carousel.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { classNames } from "../styles";
+
+export const Carousel = ({
+  data,
+}: {
+  data: { title: string; content: React.ReactNode }[];
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0); // State to manage current index
+  const [touchStartX, setTouchStartX] = useState<null | number>(null); // State to store initial touch position
+
+  // Function to handle next button click
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+  };
+
+  // Function to handle previous button click
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length);
+  };
+
+  // Touch swipe event handlers
+  const handleTouchStart = (currentX: number) => {
+    if (data.length < 2) {
+      return;
+    }
+    setTouchStartX(currentX);
+  };
+
+  const handleTouchMove = (currentX: number) => {
+    if (data.length < 2) {
+      return;
+    }
+    if (touchStartX === null) {
+      return;
+    }
+    const difference = touchStartX - currentX;
+
+    // Swipe left
+    if (difference > 5) {
+      nextSlide();
+    }
+
+    // Swipe right
+    if (difference < -5) {
+      prevSlide();
+    }
+
+    setTouchStartX(null);
+  };
+
+  return (
+    <div className="container mx-auto rounded-2xl border border-gray-200 bg-gray-100 p-4 px-4 ">
+      <div className="relative">
+        <div
+          className={classNames(
+            "mb-4 flex items-center",
+            data.length > 1 ? "justify-between" : "justify-center"
+          )}
+        >
+          {data.length > 1 && (
+            <button
+              onClick={prevSlide}
+              className="text-black focus:outline-none"
+            >
+              {"<"}
+            </button>
+          )}
+          <div className="text-sm font-bold">{data[currentIndex].title}</div>
+          {data.length > 1 && (
+            <button
+              onClick={nextSlide}
+              className="text-black focus:outline-none"
+            >
+              {">"}
+            </button>
+          )}
+        </div>
+        <div
+          onTouchStart={(e) => {
+            handleTouchStart(e.touches[0].clientX);
+          }}
+          onTouchMove={(e) => {
+            handleTouchMove(e.touches[0].clientX);
+          }}
+          onTouchEnd={() => setTouchStartX(null)}
+        >
+          {data[currentIndex].content}
+        </div>
+      </div>
+    </div>
+  );
+};
